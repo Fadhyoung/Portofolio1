84dd900eaedba911b869999e128a420cc09e05eb
Fadhli Nur Himawan <fadhlinurhimawan@gmail.com>
2025-05-02 14:35:04 +0700

    data: add data

--- src/LanguageContext.js
+++ src/LanguageContext.js
@@ -4,13 +4,13 @@ const LanguageContext = createContext();
 
 const LanguageProvider = ({ children }) => {
 
-    const [language, setLanguage] = useState("en"); // Default to English
-    const [languageData, setLanguageData] = useState({ // initiate the object first, before initiate useEffect
-        Project: [], // Add the Project array here
-        Banner: [],  // Add the Banner array here
-        Education: [], // Add the Education array here
-        About_me: {}, // Add the aboutme object here
-        Social_Media: [] // Add the social media object here
+    const [language, setLanguage] = useState("en");
+    const [languageData, setLanguageData] = useState({
+        Project: [],
+        Banner: [],
+        Education: [],
+        About_me: {},
+        Social_Media: []
       });    
 
     const fetchLanguageData = useCallback(() => {
